4537491354b2849e9cd231415d85d7ce87f18fdc
Bojsa <nebojsavasic97@gmail.com>
2025-02-03 16:42:53 +0100

    Fix and registeration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,17 @@ import { LoginComponent } from "./login/login.component";
 import { HeaderComponent } from "./Components/header/header.component";
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './Components/footer/footer.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, HeaderComponent, CommonModule, FooterComponent],
+  imports: [RouterOutlet, LoginComponent, HeaderComponent, CommonModule, FooterComponent, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'vezbanje';
+  title = 'MV drvo';
   
 }
---
+++ src/app/loginto/loginto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginto',
+  standalone: true,
+  imports: [],
+  templateUrl: './loginto.component.html',
+  styleUrl: './loginto.component.css'
+})
+export class LogintoComponent {
+
+}

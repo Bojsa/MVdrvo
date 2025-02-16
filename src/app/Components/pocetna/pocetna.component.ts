644a933ecf5d7a872d37590d6638f82dbf410852
Bojsa <nebojsavasic97@gmail.com>
2025-02-16 18:11:11 +0100

    Pocetna

--- src/app/Components/pocetna/pocetna.component.ts
+++ src/app/Components/pocetna/pocetna.component.ts
@@ -1,14 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-pocetna',
   standalone: true,
   imports: [RouterModule,CommonModule],
   templateUrl: './pocetna.component.html',
-  styleUrl: './pocetna.component.css'
+  styleUrl: './pocetna.component.css',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('1s', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class PocetnaComponent {
-
+  showElement = true;
 }
